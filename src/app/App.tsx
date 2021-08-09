2bb886f23f1f948d20cc7fe1b216124982bc22cc
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-08-09 17:55:49 +0300

    fix: fix java.io.IOException: Unable to rename

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import { Header } from 'UI/header';
+import { Header } from 'UI/Header';
 import { CreateUserForm } from 'features/users/create-user/forms';
 
 import { blueTheme } from 'styles/themes/blueTheme';

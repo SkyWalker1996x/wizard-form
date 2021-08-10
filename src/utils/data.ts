0076a0fbd20ec6ab7d8c63784ece34efbf5955f3
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-08-10 14:29:43 +0300

    feat: add util for transform array to select data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,4 @@
-export const transformToSelectOptions = (obj: { [key: string]: string }) => {
+export const transformObjToSelectOptions = (obj: { [key: string]: string }) => {
   let arr: Array<{ value: string; label: string }> = [];
 
   Object.keys(obj).forEach(key => {
@@ -6,4 +6,10 @@ export const transformToSelectOptions = (obj: { [key: string]: string }) => {
   });
 
   return arr;
+};
+
+export const transformArrToSelectOptions = (arr: string[]) => {
+  return arr.map(item => {
+    return { value: item, label: item };
+  });
 };

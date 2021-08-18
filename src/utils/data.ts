8c930ef8de89b5d729bd3d408a19098aecb208e1
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-08-18 11:33:30 +0300

    feat: add util for comparing form initialValues and current form values

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,5 @@
+import { FixTypeLater } from 'types';
+
 export const transformObjToSelectOptions = (obj: { [key: string]: string }) => {
   let arr: Array<{ value: string; label: string }> = [];
 
@@ -12,4 +14,24 @@ export const transformArrToSelectOptions = (arr: string[]) => {
   return arr.map(item => {
     return { value: item, label: item };
   });
+};
+
+const isObject = (obj: FixTypeLater) => {
+  return obj !== null && typeof obj === 'object';
+};
+
+export const extractModifiedProperties = (obj1: FixTypeLater, obj2: FixTypeLater) => {
+  let modifiedProperties = {};
+
+  Object.keys(obj1).forEach(key => {
+    if (isObject(obj1[key]) && isObject(obj2[key])) {
+      if (JSON.stringify(obj1[key]) !== JSON.stringify(obj2[key])) {
+        modifiedProperties = { ...modifiedProperties, [key]: obj2[key] };
+      }
+    } else if (obj1[key] !== obj2[key]) {
+      modifiedProperties = { ...modifiedProperties, [key]: obj2[key] };
+    }
+  });
+
+  return modifiedProperties;
 };

8a3e79e41dc11944455c49aa986544ac529fb172
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-08-23 12:59:30 +0300

    feat: implement nested routing in user page

--- src/features/user/user-info/index.tsx
+++ src/features/user/user-info/index.tsx
@@ -1,6 +1,7 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
+import { Route, Switch } from 'react-router-dom';
 
 import { fetchUser, selectUser, selectUserStatus } from '../userSlice';
 
@@ -19,16 +20,14 @@ interface UserPageMatchParams {
 }
 
 export const UserInfoPage = () => {
-  const [activeStep, setActiveStep] = useState<null | number>(null);
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
   const status = useAppSelector(selectUserStatus);
   const match = useRouteMatch<UserPageMatchParams>('/user/:id');
+  const path = match?.path;
+  const url = match?.url;
+  const isExact = match?.isExact;
   const id = match?.params.id;
-
-  const handleChangeActiveStep = useCallback((value: null | number) => {
-    setActiveStep(value);
-  }, []);
 
   useEffect(() => {
     id && dispatch(fetchUser(id));
@@ -45,22 +44,20 @@ export const UserInfoPage = () => {
   return (
     <FlexWrapper flexDirection="column" alignItems="center">
       <HeaderUserPageWrapper>
-        {activeStep ? (
-          <FlexWrapper
-            alignItems="center"
-            columnGap="10px"
-            onClick={() => handleChangeActiveStep(null)}
-          >
-            <img src={arrowLeftMark} alt="to user list" />
+        {!isExact ? (
+          <Link to={`/user/${id}`}>
+            <FlexWrapper alignItems="center" columnGap="10px">
+              <img src={arrowLeftMark} alt="to user list" />
 
-            <Text
-              text={'User Profile'}
-              textAlign="center"
-              fontWeight="700"
-              fontSize="24px"
-              color="gray100"
-            />
-          </FlexWrapper>
+              <Text
+                text={'User Profile'}
+                textAlign="center"
+                fontWeight="700"
+                fontSize="24px"
+                color="gray100"
+              />
+            </FlexWrapper>
+          </Link>
         ) : (
           <Link to="/">
             <FlexWrapper alignItems="center" columnGap="10px">
@@ -79,7 +76,7 @@ export const UserInfoPage = () => {
 
         <Text
           // @ts-ignore
-          text={activeStep ? 'Editing' : user?.username}
+          text={!isExact ? 'Editing' : user?.username}
           textAlign="center"
           fontWeight="700"
           fontSize="35px"
@@ -87,16 +84,27 @@ export const UserInfoPage = () => {
         />
       </HeaderUserPageWrapper>
 
-      {!activeStep && user && (
-        <UserInfo user={user} handleChangeActiveStep={handleChangeActiveStep} />
-      )}
-      {activeStep && user && (
-        <EditUserForm
-          activeStep={activeStep}
-          initialValues={user}
-          handleChangeActiveStep={handleChangeActiveStep}
-        />
-      )}
+      <Switch>
+        <Route exact path={path}>
+          {user && url && <UserInfo user={user} url={url} />}
+        </Route>
+
+        <Route exact path={`${path}/edit-account`}>
+          {user && <EditUserForm activeStep={1} initialValues={user} />}
+        </Route>
+
+        <Route exact path={`${path}/edit-profile`}>
+          {user && <EditUserForm activeStep={2} initialValues={user} />}
+        </Route>
+
+        <Route exact path={`${path}/edit-contacts`}>
+          {user && <EditUserForm activeStep={3} initialValues={user} />}
+        </Route>
+
+        <Route exact path={`${path}/edit-capabilities`}>
+          {user && <EditUserForm activeStep={4} initialValues={user} />}
+        </Route>
+      </Switch>
     </FlexWrapper>
   );
 };

d41463afd02103795b0f169f6f355c7b0b7272f2
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-08-25 17:44:08 +0300

    feat: add validation for phone-mask

--- src/features/user/create-user/forms/Contacts/validation.ts
+++ src/features/user/create-user/forms/Contacts/validation.ts
@@ -1,6 +1,8 @@
 import { IContactsForm } from 'types/users';
 import { FormikErrors } from 'formik';
 
+let phoneRegExp = /^\+\d{2} \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
+
 export const validate = async (values: IContactsForm) => {
   let errors: FormikErrors<IContactsForm> = {};
 
@@ -10,6 +12,20 @@ export const validate = async (values: IContactsForm) => {
 
   if (!values.mainLang) {
     errors.mainLang = 'Required';
+  }
+
+  if (values.phones.length !== 0) {
+    const phonesErrors: Array<string> = [];
+
+    values.phones.forEach((item, index) => {
+      if (item.trim() !== '' && !phoneRegExp.test(item)) {
+        phonesErrors[index] = 'Incorrect number';
+      }
+    });
+
+    if (phonesErrors.length > 0) {
+      errors.phones = phonesErrors;
+    }
   }
 
   return errors;
